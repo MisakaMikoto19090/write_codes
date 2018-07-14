a1dd4afa596a88e5622466bd76bf1d092f9a3131
Misaka <weiruancaibi@outlook.com>
2018-07-14 10:36:49 +0800

    upgrade

--- LeetCode/2/Python/如果是list.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        return list(str(int(''.join(map(lambda x:str(x),l1.val)))+int(''.join(map(lambda x:str(x),l2.val))))[::-1])
-# 不过无法通过测试,
