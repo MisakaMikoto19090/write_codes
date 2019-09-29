21f6144b1a6e27c317758a5062bf213685faf38b
MADAONOMORE <weiruancaibi@outlook.com>
2019-09-29 10:49:28 +0800

    更换vs来debug

---
+++ LeetCode/11.Container_With_Most_Water/PHP/11.19.09.23.1.php
@@ -0,0 +1,13 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer[] $height
+     * @return Integer
+     */
+    function maxArea($height) {
+        //1,100,2,1,1,1,1,1===>7 1*(7-1)
+
+
+    }
+}
---
+++ info.php
@@ -0,0 +1,5 @@
+<?php
+echo 10;
+echo 2;
+echo 3;
+phpinfo();
